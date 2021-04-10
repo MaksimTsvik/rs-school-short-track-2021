8c3268bc3b41284163ef19233249d806ddcfbf40
MaksimTsvik <mkdemix@tut.by>
2021-04-11 00:13:32 +0300

    feat: add 10th task solution

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,21 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  // throw new Error('Not implemented');
+  let smallerInd = 0;
+  let biggerInd = array.length - 1;
+  let middleInd = Math.floor((smallerInd + biggerInd) / 2);
+
+  while (array[middleInd] !== value) {
+    if (array[middleInd] > value) {
+      biggerInd = middleInd - 1;
+    } else {
+      smallerInd = middleInd + 1;
+    }
+    middleInd = Math.floor((smallerInd + biggerInd) / 2);
+  }
+  return middleInd;
 }
 
 module.exports = findIndex;
